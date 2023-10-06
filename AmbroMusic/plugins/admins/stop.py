41f9942b2b0c492da7f743a381f9e10e94de915d
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-06 18:58:11 +0300

    Update stop.py

--- AmbroMusic/plugins/admins/stop.py
+++ AmbroMusic/plugins/admins/stop.py
@@ -22,7 +22,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 
 
 @app.on_message(
-    filters.command(STOP_COMMAND)
+    filters.command(["STOP_COMMAND"]) | filters.command(["ايقاف","انهاء"],prefixes= ["/",""])
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
