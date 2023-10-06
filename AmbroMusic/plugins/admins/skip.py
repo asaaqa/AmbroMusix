a71be1b702a1a3883788f84e4c1af31415baae51
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-06 19:03:45 +0300

    Update skip.py

--- AmbroMusic/plugins/admins/skip.py
+++ AmbroMusic/plugins/admins/skip.py
@@ -28,7 +28,7 @@ SKIP_COMMAND = get_command("SKIP_COMMAND")
 
 
 @app.on_message(
-    filters.command(SKIP_COMMAND)
+    filters.command(["SKIP_COMMAND"]) | filters.command(["التالي","تخطي"],prefixes= ["/",""])
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
