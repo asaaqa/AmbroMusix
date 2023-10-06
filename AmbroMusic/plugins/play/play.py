fdc6cfd243cb1f511ea8f0e3d56ce84e2522bcd6
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-06 18:10:04 +0300

    Update play.py

--- AmbroMusic/plugins/play/play.py
+++ AmbroMusic/plugins/play/play.py
@@ -11,6 +11,7 @@ import random
 import string
 from ast import ExceptHandler
 
+from strings.filters import command
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
                             Message)
