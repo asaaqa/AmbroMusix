8da52b4139abca62fba62db34c4bbe457665fddb
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-06 16:13:46 +0300

    Update Youtube.py

--- AmbroMusic/platforms/Youtube.py
+++ AmbroMusic/platforms/Youtube.py
@@ -18,8 +18,8 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from YukkiMusic.utils.database import is_on_off
-from YukkiMusic.utils.formatters import time_to_seconds
+from AmbroMusic.utils.database import is_on_off
+from AmbroMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
