d2ab37f32e50cfdfeddd94b45e4a287d20ae6702
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-22 01:23:02 +0300

    Update stop.py

--- AmbroMusic/plugins/admins/stop.py
+++ AmbroMusic/plugins/admins/stop.py
@@ -13,7 +13,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from AmbroMusic import app
-from AmbroMusic.core.call import Yukki
+from AmbroMusic.core.call import Ambro
 from AmbroMusic.utils.database import set_loop
 from AmbroMusic.utils.decorators import AdminRightsCheck
 
@@ -31,7 +31,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Yukki.stop_stream(chat_id)
+    await Ambro.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
