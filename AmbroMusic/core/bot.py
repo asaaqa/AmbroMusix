15f3765d2b5f4970b254965491b1f54091bf910f
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-22 01:07:09 +0300

    Update bot.py

--- AmbroMusic/core/bot.py
+++ AmbroMusic/core/bot.py
@@ -17,7 +17,7 @@ import config
 from ..logging import LOGGER
 
 
-class YukkiBot(Client):
+class AmbroBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
