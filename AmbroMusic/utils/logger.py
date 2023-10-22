28aad907a3f0e30d78b654d6fe31f37766a550c3
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-22 04:49:50 +0300

    Update logger.py

--- AmbroMusic/utils/logger.py
+++ AmbroMusic/utils/logger.py
@@ -1,40 +1,31 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/AmbroMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/AmbroMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+from pyrogram.enums import ParseMode
 
-from config import LOG, LOG_GROUP_ID
 from AmbroMusic import app
 from AmbroMusic.utils.database import is_on_off
+from config import LOG_GROUP_ID
 
 
 async def play_logs(message, streamtype):
-    if await is_on_off(LOG):
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "Private Group"
+    if await is_on_off(2):
         logger_text = f"""
-**تم تشغيل عن طريق البوت**
+<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
-**الجروب:** {message.chat.title} [`{message.chat.id}`]
-**الرابط:** {message.from_user.mention}
-**اليوزر:** @{message.from_user.username}
-**الايدي:** `{message.from_user.id}`
-**رابط الحروب:** {chatusername}
+<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
+<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
 
-**بعنوان:** {message.text}
+<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
+<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
+<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
 
-**نوع التشغيل:** {streamtype}"""
+<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
-                    LOG_GROUP_ID,
-                    f"{logger_text}",
+                    chat_id=LOG_GROUP_ID,
+                    text=logger_text,
+                    parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
                 )
             except:
