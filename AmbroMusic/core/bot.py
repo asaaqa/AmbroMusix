20befca3e8c7ef7a2e8d0db541373aaed438ce4d
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-22 05:23:44 +0300

    Update bot.py

--- AmbroMusic/core/bot.py
+++ AmbroMusic/core/bot.py
@@ -35,19 +35,18 @@ class AmbroBot(Client):
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
             sys.exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-            )
-            sys.exit()
-
+        except:
+                pass
+        else:
+            pass
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
             )
             sys.exit()
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
-
-    async def stop(self):
-        await super().stop()
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
