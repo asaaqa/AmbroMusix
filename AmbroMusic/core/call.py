87f35e5a0deff61332f25a833dbe9e63b175c00f
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-22 04:00:07 +0300

    Update call.py

--- AmbroMusic/core/call.py
+++ AmbroMusic/core/call.py
@@ -45,45 +45,6 @@ from AmbroMusic.utils.inline.play import (stream_markup,
 from AmbroMusic.utils.stream.autoclear import auto_clean
 from AmbroMusic.utils.thumbnails import gen_thumb
 
-import asyncio
-import os
-from datetime import datetime, timedelta
-from typing import Union
-
-from pyrogram import Client
-from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls, StreamType
-from pytgcalls.exceptions import (
-    AlreadyJoinedError,
-    NoActiveGroupCall,
-    TelegramServerError,
-)
-from pytgcalls.types import Update
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
-from pytgcalls.types.stream import StreamAudioEnded
-
-import config
-from DAXXMUSIC import LOGGER, YouTube, app
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils.database import (
-    add_active_chat,
-    add_active_video_chat,
-    get_lang,
-    get_loop,
-    group_assistant,
-    is_autoend,
-    music_on,
-    remove_active_chat,
-    remove_active_video_chat,
-    set_loop,
-)
-from DAXXMUSIC.utils.exceptions import AssistantErr
-from DAXXMUSIC.utils.formatters import check_duration, seconds_to_min, speed_converter
-from DAXXMUSIC.utils.inline.play import stream_markup
-from DAXXMUSIC.utils.stream.autoclear import auto_clean
-from DAXXMUSIC.utils.thumbnails import get_thumb
-from strings import get_string
 
 autoend = {}
 counter = {}
