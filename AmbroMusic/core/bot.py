5fe853a8245e00f0fea5e41ee4f677d0ff83ee62
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-22 05:28:43 +0300

    Update bot.py

--- AmbroMusic/core/bot.py
+++ AmbroMusic/core/bot.py
@@ -10,12 +10,10 @@ class AmbroBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="AmbroMusic",
+            "AmbroMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            parse_mode=ParseMode.HTML,
-            max_concurrent_transmissions=7,
         )
 
     async def start(self):
